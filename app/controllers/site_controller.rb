e4a51a0fa056eaae3975a31b30419e1af64897fb
Jose Toscano <josetoscanogil@gmail.com>
2016-11-14 11:13:18 +0100

    Added calculator add function

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,4 +4,13 @@ class SiteController < ApplicationController
 
 	def contact
 	end
+
+	def calculator
+	end
+
+	def calculate
+		@first_num  = params[:first_num].to_f
+		@second_num = params[:second_num].to_f
+		@result		= @first_num + @second_num
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   get '/', to: 'site#home'
   get '/contact', to: 'site#contact'
+  get '/calculator', to: 'site#calculator'
+  post 'calculate', to: 'site#calculate'
 
 end
