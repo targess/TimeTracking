b6418afe79f518354c321381d611888769b7ed93
Jose Toscano <josetoscanogil@gmail.com>
2016-11-15 12:50:11 +0100

    list of latest projects

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   get '/', to: 'site#home'
   get '/contact', to: 'site#contact'
+  
   get '/calculator', to: 'site#calculator'
   post 'calculate', to: 'site#calculate'
 
+  get '/projects', to: 'projects#index'
 end
