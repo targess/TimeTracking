3432016b5fdd041fd88efa6bc31c188edd276de1
Jose Toscano <josetoscanogil@gmail.com>
2016-11-15 13:13:19 +0100

    Seeds generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create!(name: 'Ironhack', description: 'Ironhack is a...')
+Project.create!(name: 'Time tracking app')
+Project.create!(name: 'Recipes', description: 'Track my favorite recipes')
+
+10.times do |i|
+	Project.create!(name: "Ironhack #{i}", description: "my description #{i}")
+end
