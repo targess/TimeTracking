8277291347f6f8ca59a812dcb58d37621dd85a31
Jose Toscano <josetoscanogil@gmail.com>
2016-11-16 10:39:22 +0100

    new projects view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,4 +9,16 @@ class ProjectsController < ApplicationController
 			render "project_not_found"
 		end
 	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new(name: params[:project][:name],
+			description: params[:project][:description])
+		@project.save
+
+		redirect_to "/projects/#{@project.id}"
+	end
 end
