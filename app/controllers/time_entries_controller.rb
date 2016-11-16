436e0dda10468ab62fc86ea6c03cbc3d8b48c362
Jose Toscano <josetoscanogil@gmail.com>
2016-11-16 14:15:50 +0100

    new entries

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -3,4 +3,9 @@ class TimeEntriesController < ApplicationController
 		@project = Project.find(params[:project_id])
 		@entries = @project.time_entries.where('date > ?', 1.month.ago.end_of_month)
 	end
+
+	def new
+		@my_project = Project.find(params[:project_id])
+		@my_entry = @my_project.time_entries.new
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   post '/projects', to: 'projects#create'
 
   get '/projects/:project_id/time_entries', to: 'time_entries#index'
+  get '/projects/:project_id/time_entries/new', to: 'time_entries#new'
+  post '/projects/:project_id/time_entries', to: 'time_entries#create', as: :project_time_entries
 end
