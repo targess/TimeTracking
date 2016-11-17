30c13ae7c68a834ad0053240c9528c4d50deec64
Jose Toscano <josetoscanogil@gmail.com>
2016-11-17 12:35:00 +0100

    resources likes routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,20 @@ Rails.application.routes.draw do
   get '/calculator', to: 'site#calculator'
   post 'calculate', to: 'site#calculate'
 
-  get '/projects', to: 'projects#index'
-  get '/projects/new', to: 'projects#new'
-  get '/projects/:id', to: 'projects#show'
+  resources :projects, only: [:index, :show, :new, :create] do
+    resources :time_entries, except: [:show]
+  end
 
-  post '/projects', to: 'projects#create'
+  # get '/projects', to: 'projects#index'
+  # get '/projects/new', to: 'projects#new'
+  # get '/projects/:id', to: 'projects#show'
+  # post '/projects', to: 'projects#create'
 
-  get '/projects/:project_id/time_entries', to: 'time_entries#index'
-  get '/projects/:project_id/time_entries/new', to: 'time_entries#new'
-  post '/projects/:project_id/time_entries', to: 'time_entries#create', as: :project_time_entries
+  # get '/projects/:project_id/time_entries', to: 'time_entries#index'
+  # get '/projects/:project_id/time_entries/new', to: 'time_entries#new'
+  # post '/projects/:project_id/time_entries', to: 'time_entries#create', as: :project_time_entries
 
-  get '/projects/:project_id/time_entries/:id/edit', to: 'time_entries#edit', as: :project_time_entry_edit
-  patch '/projects/:project_id/time_entries/:id', to: 'time_entries#update', as: :project_time_entry
-  delete '/projects/:project_id/time_entries/:id', to: 'time_entries#destroy'
+  # get '/projects/:project_id/time_entries/:id/edit', to: 'time_entries#edit', as: :project_time_entry_edit
+  # patch '/projects/:project_id/time_entries/:id', to: 'time_entries#update', as: :project_time_entry
+  # delete '/projects/:project_id/time_entries/:id', to: 'time_entries#destroy'
 end
